b9607e13e46a117d18c573ce4cb6ad5b0caedfeb
Anishharur <112765855+Anishharur@users.noreply.github.com>
2024-02-02 11:08:58 +0530

    Update streamlit_app.py
    menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,12 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-# streamlit.header('Breakfast Menu')
-# streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-# streamlit.text('🥗kale, Spinach & Rocket Smoothie')
-# streamlit.text('🐔Hard-Bolied Free-Range Egg')
-# streamlit.text('🥑🍞Avocado Toast')
-# streamlit.text('🍌🥭fruit bowl🥝🍇')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Bolied Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+streamlit.text('🍌🥭fruit bowl🥝🍇')
 
 
 # import pandas
