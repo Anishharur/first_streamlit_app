20979c51f008aea201fba19fbe359b593754ab6a
Anishharur <112765855+Anishharur@users.noreply.github.com>
2024-02-03 22:24:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
   
@@ -35,12 +35,10 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    back_from_function =get_fruityvice_data(fruit_choice) 
+    back_from_function = get_fruityvice_data(fruit_choice) 
     streamlit.dataframe(back_from_function)
     
     # streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-  streamlit.error()
   
 # streamlit.write('The user entered ', fruit_choice)
 # streamlit.text(fruityvice_response.json()) #json output
@@ -66,6 +64,9 @@ if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
+
+except URLError as e:
+  streamlit.error()
   
 streamlit.stop()
 # adding new fruit
